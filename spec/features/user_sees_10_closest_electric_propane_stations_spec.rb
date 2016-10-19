78f79d117179a0242edd8e0a5f9bb7cdb221ef5b
Chase Dunagan <cdunagan@utexas.edu>
2016-10-19 09:46:01 -0600

    got rid of migrations

--- spec/features/user_sees_10_closest_electric_propane_stations_spec.rb
+++ spec/features/user_sees_10_closest_electric_propane_stations_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 describe "When I enter a zipcode" do
   scenario "I see list of top 10 closest stations" do
-    As a user
-    When I visit "/"
-    And I fill in the search form with 80203
-    And I click "Locate"
-    Then I should be on page "/search" with parameters visible in the url
+    visit "/"
+    fill_in 'q', :with => 80203
+    click_on 'Locate'
+
+    expect(current.path).to eq("/search")
+
+    expect(page).to have_content()
+
     Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     And the stations should be limited to Electric and Propane
     And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
