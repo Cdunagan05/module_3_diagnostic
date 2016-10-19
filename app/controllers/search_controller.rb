d726a8df29d27d08b67a4de5b9e7c9f18eb8027c
Chase Dunagan <cdunagan@utexas.edu>
2016-10-19 10:41:00 -0600

    made faraday call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,13 @@
 class SearchController < ApplicationController
   def index
-    @stations = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{API_KEY}"
-
+    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['API_KEY']}"
+    response_json = JSON.parse(response.body)
+    fuel = response_json["fuel_stations"]
+    #fuel is an array
+    fuel[0]["station_name"] = station_name
+    fuel[0]["street_address"] = street_address
+    fuel[0]["fuel_type_code"] = fuel_type
+    fuel[0]["access_days_time"] = access_days_time
   end
 
 end
