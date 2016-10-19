972089476c1146e32e4138d13c6945d833d31992
Chase Dunagan <cdunagan@utexas.edu>
2016-10-19 09:26:17 -0600

    set up test, create migrations for database

--- Gemfile
+++ Gemfile
@@ -8,7 +8,17 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
+gem 'figaro'
+gem 'faraday'
 gem 'sdoc', '~> 0.4.0', group: :doc
+
+group :test do
+  gem 'capybara'
+  gem 'launchy'
+  gem 'vcr'
+  gem 'webmock'
+
+end
 
 group :development, :test do
   gem 'byebug'
---
+++ spec/features/user_sees_10_closest_electric_propane_stations_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "When I enter a zipcode" do
+  scenario "I see list of top 10 closest stations" do
+    As a user
+    When I visit "/"
+    And I fill in the search form with 80203
+    And I click "Locate"
+    Then I should be on page "/search" with parameters visible in the url
+    Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    And the stations should be limited to Electric and Propane
+    And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
+end
